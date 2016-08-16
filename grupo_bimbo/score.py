b2b0aafef21981cd4c54c86fe5dc3da23f820b03
tbonza <tylers.pile@gmail.com>
2016-08-15 20:38:31 -0400

    made some initial submissions to kaggle, working through the evaluation metric now.

---
+++ grupo_bimbo/score.py
@@ -0,0 +1,13 @@
+""" We want to use the evaluation metric to anticipate our score. 
+
+
+Our dependent variable is "Demanda_uni_equil". Our evaluation metric
+is Root Mean Squared Logarithmic Error.  
+
+See: https://www.kaggle.com/wiki/RootMeanSquaredLogarithmicError
+https://www.kaggle.com/c/grupo-bimbo-inventory-demand/details/evaluation
+
+Reference for function is `rmsle.py`.
+"""
+
+
